b91a5a030869d75d7e4147bf2877f7bf8809e24d
Michael Klishin <michael@clojurewerkz.org>
2013-12-03 22:19:55 +0400

    Don't test against 1.3

--- project.clj
+++ project.clj
@@ -2,15 +2,14 @@
   :description "A yet another consistent hashing library for Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clojurewerkz/support "0.7.0-beta2"]]
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.6.1"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.5:dev,1.6:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.6:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
