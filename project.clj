4a4dc58c5fe4465f0a45339d52040292df84b290
Michael Klishin <michael@clojurewerkz.org>
2013-12-03 22:20:15 +0400

    Clojure 1.5 by default

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject clojurewerkz/chash "1.1.0-SNAPSHOT"
   :description "A yet another consistent hashing library for Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojurewerkz/support "0.7.0-beta2"]]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.6.1"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.6:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
